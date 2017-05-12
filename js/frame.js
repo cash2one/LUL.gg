2eb84082f434c2c9df19890233adff4f4912d55f
15ay3 <andus401@gmail.com>
2017-05-12 04:19:55 -0400

    added frame

---
+++ js/frame.js
@@ -0,0 +1,28 @@
+(function () {
+
+      var remote = require('electron').remote;
+
+     function init() {
+          document.getElementById("min-btn").addEventListener("click", function (e) {
+               var window = remote.getCurrentWindow()
+               window.minimize();
+          });
+
+          document.getElementById("max-btn").addEventListener("click", function (e) {
+               var window = remote.getCurrentWindow()
+               window.maximize();
+          });
+
+          document.getElementById("close-btn").addEventListener("click", function (e) {
+               var window = remote.getCurrentWindow()
+               window.close();
+          });
+     };
+
+     document.onreadystatechange = function () {
+          if (document.readyState == "complete") {
+               init();
+          }
+     };
+
+})();
