ed2d9148bb644fb8aabaaf27618dcca520950074
15ay3 <andus401@gmail.com>
2017-05-12 18:23:02 -0400

    able to look up summoners by name and fetches basic summoner info

--- js/info.js
+++ js/info.js
@@ -1,19 +1,34 @@
-var url = 'https://na.api.riotgames.com/api/lol/NA/v1.3/stats/by-summoner/39129642/ranked?season=SEASON2017&api_key=RGAPI-795e904f-77c3-4914-968d-16794eb87d4f';
-
 var summoner;
 var id;
+var icon;
 var key = 'RGAPI-795e904f-77c3-4914-968d-16794eb87d4f';
 
 function setup(){
 	loadJSON(url,gotData);
 }
 
+function getFormData(){
+	var x = document.getElementById("summoner_Search");
+	summoner = x.elements["summoner_Name"].value;
+	getBasicSummonerInfo(summoner);
+}
+
+function getBasicSummonerInfo(s){
+	var basicInfoURL = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" +summoner+"?api_key=" + key;
+	loadJSON(basicInfoURL, gotBasicSumData);
+}
+
+function gotBasicSumData(data){
+  var json = JSON.parse(data);
+	id = json[summoner].id;
+	icon = json[summoner].profileIconId;
+}
 
 function loadJSON(url, callback) {
 
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Mooolestor?api_key=RGAPI-795e904f-77c3-4914-968d-16794eb87d4f', true); // Replace 'my_data' with the path to your file
+    xobj.open('GET', url, true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
@@ -22,8 +37,3 @@ function loadJSON(url, callback) {
     };
     xobj.send(null);
  }
-
-function gotData(data){
-  var json = JSON.parse(data);
-	document.write(json['mooolestor'].id);
-}
